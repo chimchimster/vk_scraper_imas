9b6bdc2723cbb5b740f8742bbd6e66ad08e0533f
chimchimster <jorddyishere@gmail.com>
2023-11-06 14:39:34 +0600

    reset

--- scarpper/queue.py
+++ scarpper/queue.py
@@ -11,6 +11,7 @@ from vk_scraper_imas.api.utils.signals import *
 from .token.rate_limits import *
 
 RATE_LIMIT: Final[int] = 3
+MAX_OFFSET_COUNT: Final[int] = 200
 
 
 async def worker(
@@ -28,15 +29,13 @@ async def worker(
         zipped_tasks = []
 
         for token in tokens:
-
             tasks = await tasks_queue.get()
 
-            for task in tasks:
-                zipped_tasks.append((task, token))
-
-            tasks_queue.task_done()
-
-            if tasks_queue.empty():
+            if tasks:
+                for task in tasks:
+                    zipped_tasks.append((task, token))
+                tasks_queue.task_done()
+            else:
                 break
 
         async_tasks = [
@@ -62,65 +61,131 @@ async def process_task(task_distributor, task, token, rate_limited, semaphore, l
         await validator.validate_state_before_request()
 
     async with (semaphore):
-        try:
+
+        if task_name == 'SubscribedToGroup':
+
             result_response = await task_distributor.call_api_method(
                 task.coroutine_name, task.user_ids, fields=task.fields, token=token,
             )
 
-            if any(
-                    map(
-                        lambda signal: isinstance(result_response, signal),
-                        [PrivateProfileSignal, PageLockedOrDeletedSignal, AuthorizationFailedSignal]
-                    )
-            ):
+            if not isinstance(result_response, dict):
                 return
 
-            has_signal = isinstance(result_response, RateLimitSignal)
-            validation_has_been_passed = True
+            count = result_response.get('response').get('count')
 
-            if task_name in rate_limited:
-                validator = APIRateLimitsValidator(task_name, token)
-                validation_has_been_passed = await validator.validate_state_after_request(signal=has_signal)
+            if count and count > MAX_OFFSET_COUNT:
 
-            if validation_has_been_passed and result_response is not None:
+                offset = count // MAX_OFFSET_COUNT
+                reminder = count % MAX_OFFSET_COUNT
 
-                mapping_response = response_model.model_validate(result_response)
+                await handle_offset_for_subscribed_to_group(
+                    task_distributor,
+                    task,
+                    token,
+                    result_response,
+                    offset,
+                    reminder,
+                )
 
-                model_handler = await define_model_handler(mapping_response, task_name)
+        else:
+            result_response = await task_distributor.call_api_method(
+                task.coroutine_name, task.user_ids, fields=task.fields, token=token,
+            )
 
-                iterator_obj = model_handler.get(type(task_model))
+        if any(
+                map(
+                    lambda signal: isinstance(result_response, signal),
+                    [PrivateProfileSignal, PageLockedOrDeletedSignal, AuthorizationFailedSignal]
+                )
+        ):
+            return
 
-                validated_models = []
+        has_signal = isinstance(result_response, RateLimitSignal)
+        validation_has_been_passed = True
 
-                for response_data in iterator_obj:
+        if task_name in rate_limited:
+            validator = APIRateLimitsValidator(task_name, token)
+            validation_has_been_passed = await validator.validate_state_after_request(signal=has_signal)
 
-                    if not response_data:
-                        return
+        if validation_has_been_passed and result_response is not None:
 
-                    validated_data = task_model.model_validate(response_data)
-                    validated_models.append(validated_data)
+            mapping_response = response_model.model_validate(result_response)
 
-                if task_name == 'VKUser':
+            model_handler = await define_model_handler(mapping_response, task_name)
 
-                    await users_handler(
-                        [
-                            json.loads(model.json()) for model in validated_models
-                        ]
-                    )
+            iterator_obj = model_handler.get(type(task_model))
 
-                elif task_name == 'SubscribedToGroup':
+            validated_models = []
 
-                    tasks = [
-                        asyncio.create_task(
-                            subscription_handler(json.loads(model.json()), task.user_ids)
-                        ) for model in validated_models
+            for response_data in iterator_obj:
+
+                if not response_data:
+                    return
+
+                validated_data = task_model.model_validate(response_data)
+                validated_models.append(validated_data)
+
+            if task_name == 'VKUser':
+
+                await users_handler(
+                    [
+                        json.loads(model.json()) for model in validated_models
                     ]
+                )
 
-                    await asyncio.gather(*tasks)
+            elif task_name == 'SubscribedToGroup':
 
-        except Exception as e:
-            if logger:
-                await logger.info(e)
+                tasks = [
+                    asyncio.create_task(
+                        subscription_handler(json.loads(model.json()), task.user_ids)
+                    ) for model in validated_models
+                ]
+
+                await asyncio.gather(*tasks)
+
+
+async def handle_offset_for_subscribed_to_group(
+        task_distributor,
+        task,
+        token: str,
+        result_response: dict,
+        offset: int,
+        reminder: int
+) -> None:
+
+    async def extend_result_response(offset_count):
+
+        result_with_offset = await task_distributor.call_api_method(
+            task.coroutine_name, task.user_ids, fields=task.fields, token=token, offset=offset_count,
+        )
+
+        result_with_offset: dict
+
+        if not result_with_offset:
+            return
+
+        response_with_offset = result_with_offset.get('response')
+
+        if not response_with_offset or isinstance(result_with_offset, RateLimitSignal):
+            return
+        else:
+
+            items = result_with_offset.get('items')
+
+            if items:
+                result_response['response']['items'].extend(items)
+
+    for _ in range(offset):
+        if result_response.get('response').get('count') == 1596:
+            print(result_response.get('response').get('count'))
+            print(len(result_response.get('response').get('items')))
+        await extend_result_response(MAX_OFFSET_COUNT)
+        await asyncio.sleep(1)
+    else:
+        if reminder:
+            await extend_result_response(reminder)
+            if result_response.get('response').get('count') == 1596:
+                print(f'reminder: {reminder}', result_response.get('response').get('count'))
 
 
 async def define_model_handler(
